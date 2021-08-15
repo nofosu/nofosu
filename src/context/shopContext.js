96b1bc4cd09dd69f77811d122c18d03a5dbda606
nofosu <nana.ofosu-osei@consciousit.co.uk>
2021-08-15 19:46:53 +0100

    master]

--- src/context/shopContext.js
+++ src/context/shopContext.js
@@ -6,8 +6,8 @@ const ShopContext = React.createContext();
 
 // Initializing a client to return content in the store's primary language
 const client = Client.buildClient({
-  domain: "your-shop-name.myshopify.com",
-  storefrontAccessToken: "your-storefront-access-token",
+  domain: "cncsit.myshopify.com",
+  storefrontAccessToken: "81c1007ee2a6c9c68443ba162a8f25c0",
 });
 
 class ShopProvider extends Component {
@@ -19,9 +19,55 @@ class ShopProvider extends Component {
     test: "test",
   };
 
+  componentDidMount() {
+    this.createCheckout();
+  }
+
+  createCheckout = async () => {
+    const checkout = await client.checkout.create();
+    console.log(checkout);
+    this.setState({ checkout: checkout });
+  };
+
+  addItemsToCheckout = async (varientId, quantity) => {
+    const lineItemsToAdd = [{ varientId, quantity: parseInt(quantity, 10) }];
+
+    const checkout = await client.checkout.addLineItems(
+      this.state.checkout.id,
+      lineItemsToAdd
+    );
+    this, this.setState({ checkout: checkout });
+  };
+
+  fetchAllProducts = async () => {
+    const products = await client.product.fetchAll();
+    this.setState({ products: products });
+  };
+
+  fetchProductWithID = async (id) => {
+    const product = await client.product.fetch(id);
+    this.setState({ product: product });
+  };
+
+  closeCart = () => {
+    this.setState({ isCartOpen: false });
+  };
+
+  openCart = () => {
+    this.setState({ isCartOpen: true });
+  };
   render() {
     return (
-      <ShopContext.Provider value={{ ...this.state }}>
+      <ShopContext.Provider
+        value={{
+          ...this.state,
+          fetchAllProducts: this.fetchAllProducts,
+          fetchProductWithID: this.fetchProductWithID,
+          closeCart: this.closeCart,
+          openCart: this.openCart,
+          addItemsToCheckout: this.addItemsToCheckout,
+        }}
+      >
         {this.props.children}
       </ShopContext.Provider>
     );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,24 @@
 // import { test } from "picomatch";
 import React, { useContext, useEffect } from "react";
 import { ShopContext } from "../context/shopContext";
+import { Container, Form, Row, Col } from "react-bootstrap";
 
 export default function HomePage() {
-  const { test } = useContext(ShopContext);
+  const { fetchAllProducts, products } = useContext(ShopContext);
+
+  useEffect(() => {
+    fetchAllProducts();
+    return () => {};
+  }, [fetchAllProducts]);
+
+  if (!products) return <div>loading..</div>;
   return (
-    <div>
-      Home
-      {test}
-    </div>
+    <Container>
+      <Row>
+        {products.map((product) => {
+          <Col key={product.id}>{product.title}</Col>;
+        })}
+      </Row>
+    </Container>
   );
 }
